6011e1f32c984841fdfc01c8a19d8145478268bc
william-fiset <fisetwill@gmail.com>
2012-05-03 19:31:02 -0300

    VIDEO TWO

--- FractalExplorer.java
+++ FractalExplorer.java
@@ -17,8 +17,21 @@ public class FractalExplorer extends JFrame {
 	static final int WIDTH  = 600;
 	static final int HEIGHT = 600;
 	
+	Canvas canvas;
+	BufferedImage fractalImage;
+	
 	public FractalExplorer() {
 		setInitialGUIProperties();
+		addCanvas();
+	}
+	
+	private void addCanvas() {
+		
+		canvas = new Canvas();
+		fractalImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		canvas.setVisible(true);
+		this.add(canvas, BorderLayout.CENTER);
+		
 	}
 	
 	public void setInitialGUIProperties() {
@@ -36,5 +49,42 @@ public class FractalExplorer extends JFrame {
 		new FractalExplorer();
 	}
 	
+	
+	private class Canvas extends JPanel {
+		
+		@Override public Dimension getPreferredSize() {
+			return new Dimension(WIDTH, HEIGHT);
+		}
+		
+		@Override public void paintComponent(Graphics drawingObj) {
+			drawingObj.drawImage( fractalImage, 0, 0, null );
+		}
+		
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
