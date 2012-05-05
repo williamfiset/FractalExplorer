51aaf3e577a4137050fdbd154edae6f19e3eff07
william-fiset <fisetwill@gmail.com>
2012-05-04 21:28:15 -0300

    VIDEO FOUR

--- FractalExplorer.java
+++ FractalExplorer.java
@@ -22,10 +22,98 @@ public class FractalExplorer extends JFrame {
 	
 	static final int MAX_ITER = 200;
 	
+	static final double DEFAULT_ZOOM       = 100.0;
+	static final double DEFAULT_TOP_LEFT_X = -3.0;
+	static final double DEFAULT_TOP_LEFT_Y = +3.0;
+	
+	double zoomFactor = DEFAULT_ZOOM;
+	double topLeftX   = DEFAULT_TOP_LEFT_X;
+	double topLeftY   = DEFAULT_TOP_LEFT_Y;
+
+// -------------------------------------------------------------------
 	public FractalExplorer() {
 		setInitialGUIProperties();
 		addCanvas();
+		updateFractal();
 	}
+	
+// -------------------------------------------------------------------
+
+	public static void main(String[] args) {
+		new FractalExplorer();
+	}
+	
+// -------------------------------------------------------------------
+
+		private void addCanvas() {
+			
+			canvas = new Canvas();
+			fractalImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+			canvas.setVisible(true);
+			this.add(canvas, BorderLayout.CENTER);
+			
+		} // addCanvas
+
+// -------------------------------------------------------------------
+		
+		private void setInitialGUIProperties() {
+			
+			this.setTitle("Fractal Explorer");
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			this.setSize(WIDTH, HEIGHT);
+			this.setResizable(false);
+			this.setLocationRelativeTo(null);
+			this.setVisible(true);
+			
+		} // setInitialGUIProperties
+		
+// -------------------------------------------------------------------
+	private double getXPos(double x) {
+		return x/zoomFactor + topLeftX;
+	} // getXPos
+// -------------------------------------------------------------------
+	private double getYPos(double y) {
+		return y/zoomFactor - topLeftY;
+	} // getYPos
+// -------------------------------------------------------------------
+	
+	/**
+	 * Updates the fractal by computing the number of iterations
+	 * for each point in the fractal and changing the color
+	 * based on that.
+	 **/
+	
+	public void updateFractal() {
+		
+		for (int x = 0; x < WIDTH; x++ ) {
+			for (int y = 0; y < HEIGHT; y++ ) {
+				
+				double c_r = getXPos(x);
+				double c_i = getYPos(y);
+				
+				int iterCount = computeIterations(c_r, c_i);
+				
+				int pixelColor = makeColor(iterCount);
+				fractalImage.setRGB(x, y, pixelColor);
+				
+			}
+		}
+		
+		canvas.repaint();
+		
+	} // updateFractal
+// -------------------------------------------------------------------	
+	private int makeColor( int iterCount ) {
+		
+		if (iterCount == MAX_ITER) {
+			return 0;//Color.BLACK.getRGB();
+		}
+		
+		return Color.BLUE.getRGB();
+		
+	} // makeColor
+
+// -------------------------------------------------------------------
 
 	private int computeIterations(double c_r, double c_i) {
 		
@@ -69,46 +157,23 @@ public class FractalExplorer extends JFrame {
 		// Complex point was outside Mandelbrot set
 		return iterCount;
 		
-	} 
+	} // computeIterations
+
+// -------------------------------------------------------------------	
 	
-	private void addCanvas() {
-		
-		canvas = new Canvas();
-		fractalImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-		canvas.setVisible(true);
-		this.add(canvas, BorderLayout.CENTER);
-		
-	}
-	
-	private void setInitialGUIProperties() {
-		
-		this.setTitle("Fractal Explorer");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(WIDTH, HEIGHT);
-		this.setResizable(false);
-		this.setLocationRelativeTo(null);
-		this.setVisible(true);
-		
-	}
-	
-	public static void main(String[] args) {
-		new FractalExplorer();
-	}
-		
 	private class Canvas extends JPanel {
 		
 		@Override public Dimension getPreferredSize() {
 			return new Dimension(WIDTH, HEIGHT);
-		}
+		} // getPreferredSize
 		
 		@Override public void paintComponent(Graphics drawingObj) {
 			drawingObj.drawImage( fractalImage, 0, 0, null );
-		}
+		} // paintComponent
 		
-	}
+	} // Canvas
 	
-	
-}
+} // FractalExplorer
 
 
 
