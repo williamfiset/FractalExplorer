6ffdd7e0a5758acf3e98bd66127b2445845ba0fa
William Fiset <fisetwill@gmail.com>
2016-10-02 15:05:51 -0300

    Fixed some indentation issues

--- FractalExplorer.java
+++ FractalExplorer.java
@@ -46,27 +46,27 @@ public class FractalExplorer extends JFrame {
 	
 // -------------------------------------------------------------------
 
-		private void addCanvas() {
-			
-			canvas = new Canvas();
-			fractalImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-			canvas.setVisible(true);
-			this.add(canvas, BorderLayout.CENTER);
-			
-		} // addCanvas
+	private void addCanvas() {
+
+		canvas = new Canvas();
+		fractalImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		canvas.setVisible(true);
+		this.add(canvas, BorderLayout.CENTER);
+
+	} // addCanvas
 
 // -------------------------------------------------------------------
-		
-		private void setInitialGUIProperties() {
-			
-			this.setTitle("Fractal Explorer");
-			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			this.setSize(WIDTH, HEIGHT);
-			this.setResizable(false);
-			this.setLocationRelativeTo(null);
-			this.setVisible(true);
-			
-		} // setInitialGUIProperties
+
+	private void setInitialGUIProperties() {
+
+		this.setTitle("Fractal Explorer");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(WIDTH, HEIGHT);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+
+	} // setInitialGUIProperties
 		
 // -------------------------------------------------------------------
 	private double getXPos(double x) {
@@ -129,12 +129,12 @@ public class FractalExplorer extends JFrame {
 		Let z = z_r + z_i
 		
 		z' = z*z + c
-		   = (z_r + z_i)(z_r + z_i) + c_r + c_i
-			 = z_r² + 2*z_r*z_i - z_i² + c_r + c_i
-			
-			 z_r' = z_r² - z_i² + c_r
-			 z_i' = 2*z_i*z_r + c_i
-		
+		   = (z_r + z_i)(z_r + z_i) + (c_r + c_i)
+		   = z_r² + 2*z_r*z_i - z_i² + c_r + c_i
+
+		     z_r' = z_r² - z_i² + c_r
+		     z_i' = 2*z_i*z_r + c_i
+		     
 		*/
 
 		double z_r = 0.0;
